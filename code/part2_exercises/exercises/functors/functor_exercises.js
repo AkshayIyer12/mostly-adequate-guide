d34776415095849151486e702068a2215593ac1e
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-09-19 22:55:39 +0530

    Add functor exercise

--- code/part2_exercises/exercises/functors/functor_exercises.js
+++ code/part2_exercises/exercises/functors/functor_exercises.js
@@ -1,19 +1,20 @@
 require('../../support')
 var Task = require('data.task')
 var _ = require('ramda')
-
+var fs = require('fs')
+var curry = _.curry
 // Exercise 1
 // ==========
 // Use _.add(x,y) and _.map(f,x) to make a function that increments a value inside a functor
 
-var ex1
+var ex1 = _.map(_.add(1))
 
 // Exercise 2
 // ==========
 // Use _.head to get the first element of the list
-var xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do'])
+var xs = Identity.of(['doo', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do'])
 
-var ex2
+var ex2 = _.map(_.head)
 
 // Exercise 3
 // ==========
@@ -22,8 +23,7 @@ var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]) })
 
 var user = { id: 2, name: 'Albert' }
 
-var ex3
-
+var ex3 = _.compose(_.map(_.head), safeProp('name'))
 // Exercise 4
 // ==========
 // Use Maybe to rewrite ex4 without an if statement
@@ -32,7 +32,7 @@ var ex4 = function (n) {
   if (n) { return parseInt(n) }
 }
 
-var ex4
+var ex4 = _.compose(_.map(parseInt), Maybe.of)
 
 // Exercise 5
 // ==========
@@ -46,8 +46,8 @@ var getPost = function (i) {
     }, 300)
   })
 }
-
-var ex5
+var upperTitle = _.compose(toUpperCase, _.prop('title'))
+var ex5 = _.compose(_.map(upperTitle), getPost)
 
 // Exercise 6
 // ==========
@@ -56,30 +56,29 @@ var ex5
 var showWelcome = _.compose(_.concat('Welcome '), _.prop('name'))
 
 var checkActive = function (user) {
-  return user.active ? Right.of(user) : Left.of('Your account is not active')
+    return user.active ? Right.of(user) : Left.of('Your account is not active')
 }
 
-var ex6
+var ex6 = _.compose(_.map(showWelcome), checkActive)
 
 // Exercise 7
 // ==========
 // Write a validation function that checks for a length > 3. It should return Right(x) if it is greater than 3 and Left("You need > 3") otherwise
 
 var ex7 = function (x) {
-  return undefined // <--- write me. (don't be pointfree)
-}
-
+    return (x.length > 3) ? Right.of(x) : Left.of('You need > 3')
+} // <--- write me. (don't be pointfree)
 // Exercise 8
 // ==========
 // Use ex7 above and either as a functor to save the user if they are valid or return the error message string. Remember either's two arguments must return the same type.
 
 var save = function (x) {
   return new IO(function () {
-    console.log('SAVED USER!')
+      console.log('SAVED USER!')
     return x + '-saved'
   })
 }
 
-var ex8
+var ex8 = _.compose(either(IO.of, save), ex7)
 
 module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, ex5: ex5, ex6: ex6, ex7: ex7, ex8: ex8}
